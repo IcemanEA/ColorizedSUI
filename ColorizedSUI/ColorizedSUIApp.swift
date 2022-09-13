af6c8dcaebc2aad60fe36adeb0a3aa94f57edcdc
Egor Ledkov <egor@ledkov.org>
2022-09-13 19:30:25 +0300

    Initial Commit

---
+++ ColorizedSUI/ColorizedSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedSUIApp.swift
+//  ColorizedSUI
+//
+//  Created by Egor Ledkov on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
